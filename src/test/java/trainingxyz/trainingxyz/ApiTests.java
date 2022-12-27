782e4df9583270f2680d009e16d8047b2ddf2d1a
Andreea-Lenuta Timar <andreea.timar@fortech.ro>
2022-12-27 15:44:45 +0200

    get multivitamins product test

--- src/test/java/trainingxyz/trainingxyz/ApiTests.java
+++ src/test/java/trainingxyz/trainingxyz/ApiTests.java
@@ -86,7 +86,7 @@ public class ApiTests {
         response.log().status();
     }
 
-       @Test
+    @Test
     public void createSerializedProduct() {
         String endpoint = "http://localhost:8888/api_testing/product/create.php";
         Product product = new Product(
@@ -182,5 +182,24 @@ public class ApiTests {
 
     }
 
+    @Test
+    public void getMultiVitamins() {
+        String endpoint = "http://localhost:8888/api_testing/product/read_one.php";
+        given().
+                queryParam("id", 18).
+                when().
+                get(endpoint).
+                then().
+                assertThat().
+                statusCode(200).
+                header("Content-Type", equalTo("application/json")).
+                body("id", equalTo("18")).
+                body("name", equalTo("Multi-Vitamin (90 capsules)")).
+                body("description", equalTo("A daily dose of our Multi-Vitamins fulfills a day’s nutritional " +
+                        "needs for over 12 vitamins and minerals.")).
+                body("price", equalTo("10.00")).
+                body("category_id", equalTo("4")).
+                body("category_name", equalTo("Supplements"));
+    }
 }
 
